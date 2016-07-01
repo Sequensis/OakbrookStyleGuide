8eb40c1fb234293db4e471ba212122a2ee9c1574
Humenko <danielhumenko@gmail.com>
2016-07-01 13:40:36 +0100

    Add navbar within styleguide

--- src/Scripts/Docs/Navbar.js
+++ src/Scripts/Docs/Navbar.js
@@ -1,11 +1,6 @@
-hljs.initHighlightingOnLoad();
 $('#navbar-icon').click(function () {
     $(this).toggleClass('icon-open');
     $(".navbar-circle").toggleClass("circle-open");
     $(".btn-application").toggleClass('btn-application-open');
     $(".btn-sign-in").toggleClass('btn-sign-in-open');
-});
-$("#menu-toggle").click(function (e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
 });

6780ce59c89f9b04dde1ead45e50e4233fa6d7b5
Humenko <danielhumenko@gmail.com>
2016-07-08 14:42:56 +0100

    Change styling of Auth0/Navbar/Footer/Loader

--- src/Scripts/Docs/Navbar.js
+++ src/Scripts/Docs/Navbar.js
@@ -1,13 +1,17 @@
 $('#navbar-icon').click(function () {
     $(this).toggleClass('icon-open');
-    // $(".navbar").toggleClass("navbar-fixed-top");
     $(".navbar-circle").toggleClass("circle-open");
     $(".btn-application").toggleClass('btn-application-open');
     $(".btn-sign-in").toggleClass('btn-sign-in-open');
+    $(".btn-sign-out").toggleClass('btn-sign-out-open');
+    $(".btn-username").toggleClass('btn-username-open');
     $(".overlay").toggleClass('overlay-open');
 });
 
-$('.btn-sign-in').click(function () {
-    $(this).toggleClass('btn-primary-1');
-    $(this).toggleClass('btn-primary-3');
+$('.btn-sign-in-example').click(function () {
+    $(".btn-sign-out").toggleClass('btn-sign-in');
+    $(".btn-sign-out").removeClass('btn-sign-out');
+    $(".btn-sign-in").text("Sign in");
+    $(".btn-username").empty();
+    $("#overlay-sign-in").empty()
 });

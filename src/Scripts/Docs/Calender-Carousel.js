889e3b13c904204ded89f9d9d14648ad70eaaa6b
Humenko <danielhumenko@gmail.com>
2016-09-13 15:29:26 +0100

    add functioning Calender-Carousel and payments components

--- src/Scripts/Docs/Calender-Carousel.js
+++ src/Scripts/Docs/Calender-Carousel.js
@@ -1,22 +1,42 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsive:{
-        0:{
-            items:4
-        },
-        600:{
-            items:7
-        },
-        1000:{
-            items:8
-        }
-    }
-})
+$(document).ready(function() {
 
- $(document).on('click', '.calendar-carousel-date', function (e) {
-   $('.calendar-carousel-date').removeClass('calendar-carousel-date-active');
-   $('.calendar-carousel-date').removeClass('large');
-   $(this).addClass('calendar-carousel-date-active');
-   $(this).addClass('large');
- });
+    var paymentCarousel = $(".owl-carousel");
+
+    paymentCarousel.owlCarousel({
+        loop: true,
+        margin: 10,
+        center:true,
+        responsive: {
+            0: {
+                items: 4
+            },
+            550: {
+                items: 7
+            },
+            768: {
+                items: 9
+            },
+            992: {
+                items: 5
+            },
+            1200: {
+                items: 6
+            }
+        }
+    });
+
+    $(".prev").click(function() {
+        paymentCarousel.trigger('prev.owl.carousel');
+    });
+
+    $(".next").click(function() {
+        paymentCarousel.trigger('next.owl.carousel');
+    });
+});
+
+$(document).on('click', '.calendar-carousel-date', function(e) {
+    $('.calendar-carousel-date').removeClass('calendar-carousel-date-active');
+    $('.calendar-carousel-date').removeClass('large');
+    $(this).addClass('calendar-carousel-date-active');
+    $(this).addClass('large');
+});
---
+++ src/Scripts/Docs/Change-Payment.js
@@ -0,0 +1,6 @@
+$(document).on('click', '#change-payment', function(e) {
+    $('#change-payment-top').toggleClass('payment-active');
+    $('#change-payment-bottom').toggleClass('payment-active');
+    $('#change-payment-dotted-line').toggleClass('payment-active');
+    $('#change-payment-active').toggleClass('payment-active');    
+});

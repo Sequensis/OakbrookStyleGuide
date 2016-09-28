50858bdd0a42a54cf4e6b1bc2f1bedcb5f97fbd1
Humenko <danielhumenko@gmail.com>
2016-09-28 17:04:42 +0100

    alter owl carousel to swiper

--- src/Scripts/Docs/Calender-Carousel.js
+++ src/Scripts/Docs/Calender-Carousel.js
@@ -1,33 +1,20 @@
-$(document).ready(function() {
-
-    var paymentCarousel = $(".owl-carousel");
-
-    paymentCarousel.owlCarousel({
-        margin: -10,
-        slideBy: 3,
-        center: true,
-        mouseDrag: false,
-        responsive: {
-            0: {
-                items: 2
-            },
-            768: {
-                items: 5
-            }
-        }
-    });
-
-    $(".prev").click(function() {
-        paymentCarousel.trigger('prev.owl.carousel');
-    });
-
-    $(".next").click(function() {
-        paymentCarousel.trigger('next.owl.carousel');
+$(document).ready(function () {
+    $('#change-payment-day').on('shown.bs.modal', function (e) {
+        swiper.update();
+    })
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+        nextButton: '.next',
+        prevButton: '.prev',
+        freeMode: true,
+        centeredSlides: true,
+        freeModeSticky: true
     });
 });
 
-$(document).on('click', '.calendar-carousel-date', function(e) {
-    $('.calendar-carousel-date').removeAttr('id','calendar-carousel-date-active');
-    $(this).attr('id','calendar-carousel-date-active');
+$(document).on('click', '.swiper-slide', function (e) {
+    $('.swiper-slide').removeAttr('id', 'calendar-carousel-date-active');
+    $(this).attr('id', 'calendar-carousel-date-active');
     $('#payment-change-effective').removeClass('invisible');
 });

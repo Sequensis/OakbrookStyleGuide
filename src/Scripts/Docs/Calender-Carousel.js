598349804ea7bc8ccd0ca561a71682b433f045b0
Humenko <danielhumenko@gmail.com>
2016-10-04 09:44:56 +0100

    update carousel js

--- src/Scripts/Docs/Calender-Carousel.js
+++ src/Scripts/Docs/Calender-Carousel.js
@@ -3,13 +3,44 @@ $(document).ready(function () {
         swiper.update();
     })
     var swiper = new Swiper('.swiper-container', {
-        slidesPerView: 'auto',
+        slidesPerView: '5',
         spaceBetween: 10,
         nextButton: '.next',
         prevButton: '.prev',
         freeMode: true,
         centeredSlides: true,
-        freeModeSticky: true
+        freeModeSticky: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 'auto',
+                spaceBetween: 10,
+            },
+        }
+    });
+
+    var paymentCarousel = $(".owl-carousel");
+
+    paymentCarousel.owlCarousel({
+        margin: -10,
+        slideBy: 3,
+        center: true,
+        mouseDrag: false,
+        responsive: {
+            0: {
+                items: 2
+            },
+            768: {
+                items: 5
+            }
+        }
+    });
+
+    $(".prev").click(function () {
+        paymentCarousel.trigger('prev.owl.carousel');
+    });
+
+    $(".next").click(function () {
+        paymentCarousel.trigger('next.owl.carousel');
     });
 });
 
@@ -18,4 +49,11 @@ $(document).on('click', '.swiper-slide', function (e) {
     $(this).attr('id', 'calendar-carousel-date-active');
     $('#payment-change-effective').removeClass('invisible');
     $('#payment-loader').addClass('d-none');
+});
+
+$(document).on('click', '#close-modal', function (e) {
+    $('#important-1').removeClass('in');
+    setTimeout(function () {
+        $('#important-1').modal('hide');
+    }, 500);
 });

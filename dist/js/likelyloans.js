f6154554165454afcba9e63737e33d672cded20b
ammar.haider <ammar.haider@oakbrookfinance.com>
2016-11-30 13:03:46 +0000

    off canvas div and dimensions

--- dist/js/likelyloans.js
+++ dist/js/likelyloans.js
@@ -4795,12 +4795,13 @@ if (typeof jQuery === 'undefined') {
 
   $(document).on('click', '#navbar-icon', function (e) {
     $(this).toggleClass('icon-open');
-    $(".navbar-circle").toggleClass("circle-open");
-    $(".btn-application").toggleClass('btn-application-open');
-    $(".btn-sign-in").toggleClass('btn-sign-in-open');
-    $(".btn-sign-out").toggleClass('btn-sign-out-open');
-    $(".btn-username").toggleClass('btn-username-open');
-    $(".overlay").toggleClass('overlay-open');
+    // $(".navbar-circle").toggleClass("circle-open");
+    // $(".btn-application").toggleClass('btn-application-open');
+    // $(".btn-sign-in").toggleClass('btn-sign-in-open');
+    // $(".btn-sign-out").toggleClass('btn-sign-out-open');
+    // $(".btn-username").toggleClass('btn-username-open');
+    // $(".overlay").toggleClass('overlay-open');
+    $('.off-canvas').toggleClass('open');
   });
 }(jQuery);
 $('.btn-warning').click(function () {

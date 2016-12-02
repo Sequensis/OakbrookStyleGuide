47ee4503a718e10a3c57eb4d249acda6426da393
ammar.haider <ammar.haider@oakbrookfinance.com>
2016-12-02 10:42:22 +0000

    re-instating old navbar css and js so new navbar is no longe ra breaking change, small tweaks to styling of new navbar

--- src/Scripts/Docs/Navbar.js
+++ src/Scripts/Docs/Navbar.js
@@ -1,25 +1,14 @@
 +function ($) {
  //'use strict';
  
- $('.navbar-off-canvas').on('click', '#navbar-icon', function (e) {
-	$(this).toggleClass('icon-open');
-	$(".navbar-circle").toggleClass("circle-open");
-	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
-	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
-	$('.mask').toggleClass('on');
-	$('.off-canvas').toggleClass('open');
-	$('body').toggleClass('noscroll');
+ $('.navbar-vertical').on('click', '#navbar-icon', function (e) {
+   $(this).toggleClass('icon-open');
+   $(".navbar-circle").toggleClass("circle-open");
+   $(".btn-application").toggleClass('btn-application-open');
+   $(".btn-sign-in").toggleClass('btn-sign-in-open');
+   $(".btn-sign-out").toggleClass('btn-sign-out-open');
+   $(".btn-username").toggleClass('btn-username-open');
+   $(".overlay").toggleClass('overlay-open');
  });
-
-// click on mask overlay
-$('.load-fade-in').on('click', '.mask.on', function() {
-	$('#navbar-icon').toggleClass('icon-open');
-	$(".navbar-circle").toggleClass("circle-open");
-	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
-	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
-	$('.mask').toggleClass('on');
-	$('.off-canvas').toggleClass('open');
-	$('body').toggleClass('noscroll');
-});
 
 }(jQuery);
---
+++ src/Scripts/Docs/NavbarOffCanvas.js
@@ -0,0 +1,25 @@
++function ($) {
+ //'use strict';
+ 
+ $('.navbar-off-canvas').on('click', '#navbar-icon', function (e) {
+	$(this).toggleClass('icon-open');
+	$(".navbar-circle").toggleClass("circle-open");
+	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
+	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
+	$('.mask').toggleClass('on');
+	$('.off-canvas').toggleClass('open');
+	$('body').toggleClass('noscroll');
+ });
+
+// click on mask overlay
+$('.load-fade-in').on('click', '.mask.on', function() {
+	$('#navbar-icon').toggleClass('icon-open');
+	$(".navbar-circle").toggleClass("circle-open");
+	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
+	$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
+	$('.mask').toggleClass('on');
+	$('.off-canvas').toggleClass('open');
+	$('body').toggleClass('noscroll');
+});
+
+}(jQuery);

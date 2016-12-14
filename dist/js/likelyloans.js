085afae3598879ad71464ab0774a612774608df3
ammar.haider <ammar.haider@oakbrookfinance.com>
2016-12-14 14:59:09 +0000

    dist js files

--- dist/js/likelyloans.js
+++ dist/js/likelyloans.js
@@ -4791,28 +4791,17 @@ if (typeof jQuery === 'undefined') {
   return window.Swiper;
 });
 +function ($) {
-	//'use strict';
+  //'use strict';
 
-	$('.navbar-off-canvas').on('click', '#navbar-icon', function (e) {
-		$(this).toggleClass('icon-open');
-		$(".navbar-circle").toggleClass("circle-open");
-		$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
-		$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
-		$('.mask').toggleClass('on');
-		$('.off-canvas').toggleClass('open');
-		$('body').toggleClass('noscroll');
-	});
-
-	// click on mask overlay
-	$('.load-fade-in').on('click', '.mask.on', function () {
-		$('#navbar-icon').toggleClass('icon-open');
-		$(".navbar-circle").toggleClass("circle-open");
-		$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade');
-		$('.navbar-logo, .navbar-btn.btn-sign-out').toggleClass('ob-fade-out');
-		$('.mask').toggleClass('on');
-		$('.off-canvas').toggleClass('open');
-		$('body').toggleClass('noscroll');
-	});
+  $('.navbar').not('.navbar-off-canvas').on('click', '#navbar-icon', function (e) {
+    $(this).toggleClass('icon-open');
+    $(".navbar-circle").toggleClass("circle-open");
+    $(".btn-application").toggleClass('btn-application-open');
+    $(".btn-sign-in").toggleClass('btn-sign-in-open');
+    $(".btn-sign-out").toggleClass('btn-sign-out-open');
+    $(".btn-username").toggleClass('btn-username-open');
+    $(".overlay").toggleClass('overlay-open');
+  });
 }(jQuery);
 $('.btn-warning').click(function () {
     $('.modal-container').toggleClass('theme-warning');

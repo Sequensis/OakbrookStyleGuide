03a315d3dbcf6428e267c3b85c6ff8c487ac560b
Humenko <humenko@DTH-MacBook.home>
2016-12-21 23:45:37 +0000

    Create top up calculation page

--- src/Scripts/Docs/Loan-Slider.js
+++ src/Scripts/Docs/Loan-Slider.js
@@ -1,21 +1,29 @@
-if (location.pathname === "/example-slider.html") {
+if (location.pathname === "/example-top-up-calc.html") {
 
-	var slider = document.getElementById('loan-slider');
+	var minVal = 1000;
+	var maxVal = 2000;
+	var loanStep = 10;
 
-	noUiSlider.create(slider, {
-		start: 2000,
-		step: 10,
+	var loanSlider = document.getElementById('loan-slider');
+
+	noUiSlider.create(loanSlider, {
+		start: maxVal - 500,
 		connect: [true, false],
+		step: loanStep,
 		range: {
-			'min': 1000,
-			'max': 5000
+			'min': minVal,
+			'max': maxVal
 		}
 	});
 
-	var stepSliderValueElement = document.getElementById('loan-slider-value');
+	var loanSliderValue = document.getElementById('loan-slider-value');
 
-	slider.noUiSlider.on('update', function (values, handle) {
-		stepSliderValueElement.innerHTML = values[handle];
+	loanSlider.noUiSlider.on('update', function (values, handle) {
+		loanSliderValue.value = values[handle];
+	});
+
+	loanSliderValue.addEventListener('change', function () {
+		loanSlider.noUiSlider.set(this.value);
 	});
 
 }

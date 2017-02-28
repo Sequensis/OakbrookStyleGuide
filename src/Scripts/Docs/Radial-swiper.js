a57b4c059f1062ba051dd55686c854d78b517f32
humenko <danielhumenko@gmail.com>
2017-02-28 12:43:05 +0000

    create radial swiper mock up

---
+++ src/Scripts/Docs/Radial-swiper.js
@@ -0,0 +1,11 @@
+if (location.pathname === "/example-new-summary.html") {
+
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        centeredSlides: true, 
+        paginationClickable: true,
+        loop: true
+        // autoplay: 5000
+    });
+
+}

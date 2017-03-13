d5616ceef7742948b1912d50b400b94fdba6e4f7
humenko <danielhumenko@gmail.com>
2017-03-13 11:32:40 +0000

    update ripple effect to only be used on specific buttons

--- src/Scripts/Docs/Ripple.js
+++ src/Scripts/Docs/Ripple.js
@@ -1,31 +1,34 @@
 var parent, ripple, d, x, y;
-$("a").click(function(e){
+$(".ob-btn-toggle").click(function (e) {
 	parent = $(this).parent();
 	//create .ripple element if it doesn't exist
-	if(parent.find(".ripple").length == 0)
+	if (parent.find(".ripple").length == 0)
 		parent.prepend("<span class='ripple'></span>");
-		
+
 	ripple = parent.find(".ripple");
 	//incase of quick double clicks stop the previous animation
 	ripple.removeClass("animate");
-	
+
 	//set size of .ripple
-	if(!ripple.height() && !ripple.width())
-	{
+	if (!ripple.height() && !ripple.width()) {
 		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
 		d = Math.max(parent.outerWidth(), parent.outerHeight());
-		ripple.css({height: d, width: d});
+		ripple.css({
+			height: d,
+			width: d
+		});
 	}
-	
+
 	//get click coordinates
 	//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-	x = e.pageX - parent.offset().left - ripple.width()/2;
-	y = e.pageY - parent.offset().top - ripple.height()/2;
-	
-	//set the position and add class .animate
-	ripple.css({top: y+'px', left: x+'px'}).addClass("animate");
-})  
+	x = e.pageX - parent.offset().left - ripple.width() / 2;
+	y = e.pageY - parent.offset().top - ripple.height() / 2;
 
-$(".ob-btn-toggle").click(function (e) { 
+	//set the position and add class .animate
+	ripple.css({
+		top: y + 'px',
+		left: x + 'px'
+	}).addClass("animate");
 	$(this).toggleClass("checked");
-});
+
+})
